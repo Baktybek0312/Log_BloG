542bb6c16051bb23365c9b31554c328197834b10
Baha <toktobekov1995@gmail.com>
2022-06-02 15:13:46 +0600

    создал файлы для получение данных url 8000/posts/list

--- core/log_config.py
+++ core/log_config.py
@@ -1,6 +1,5 @@
 import loguru
 
-
 logger = loguru.logger
 logger.remove()
 logger.add(
@@ -9,12 +8,12 @@ logger.add(
     level="DEBUG",
     enqueue=True
 )
+logger.debug('Error')
+logger.info('Information message')
+logger.warning('Warning')
 
 
 def divide(a, b):
     logger.debug(f"Dividing {a} / {b} ...")
     result = a / b
     logger.debug(f"Result is {result}")
-
-
-
--- main.py
+++ main.py
@@ -1,59 +1,15 @@
-import uuid
-import random
-
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
 
-import requests
-
-from core.log_config import logger, divide
+from routes import tasks
 
 app = FastAPI()
 
-
-@app.middleware("http")
-async def request_middleware(request, call_next):
-    request_id = str(uuid.uuid4())
-    with logger.contextualize(request_id=request_id):
-        logger.info("Request started")
-
-        try:
-            return await call_next(request)
-
-        except Exception as ex:
-            logger.error(f"Request failed: {ex}")
-            return JSONResponse(content={"success": False}, status_code=500)
-
-        finally:
-            logger.info("Request ended")
-
-
-@app.get("/")
-async def read_root():
-    a = 100
-    b = random.randint(0, 1)
-    return {"success": True, "result": divide(a, b)}
-
-
-URL = 'http://127.0.0.1:8000/posts/list'
-
-
-@app.get("/get_data")
-def get_data():
-    data = requests.get(url=URL)
-
-    with open('get_data_blog_posts.xls', 'wb') as file:
-        file.write(data.content)
-    return data.json()
-
-# @app.post("/upload")
-# async def upload(file: UploadFile = File(...)):
-#     try:
-#         contents = await file.read()
-#         with open(file.filename, 'wb') as f:
-#             f.write(contents)
-#     except Exception:
-#         return {"message": "There was an error uploading the file"}
-#     finally:
-#         await file.close()
-#     return {"message": f"Successfuly uploaded {file.filename}"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(tasks.router)
